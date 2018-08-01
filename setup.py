fc5563b49e1f784023828c7a94cdf653cfd2253c
Alec Deason <alecwd@uw.edu>
2018-08-01 12:06:02 -0700

    Missing argument to find_packages

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name="distributed_systems_talk",
     version="0.5",
-    packages=find_packages(),
+    packages=find_packages("src"),
     package_dir={"": "src"},
     include_package_data=True,
     extras_require={"testing": ["pytest", "pytest-mock"]},
